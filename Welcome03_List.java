4b3d1c9ead6e7f9c772171413edb2e1a20fec72c
ethantseng <tseng8851@mydusd.org>
2026-01-30 10:49:56 -0800

    Update Welcome03_List.java

--- Welcome03_List.java
+++ Welcome03_List.java
@@ -1,27 +1,44 @@
-/*
- * Arrays of objects
- */
-
 import core.data.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Welcome03_List {
-   public static void main(String[] args) {
+   public static void main(String[] args) {  
       DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/index.xml").load();
       ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name", 
              "station/station_id", "station/state",
              "station/latitude", "station/longitude");
-      System.out.println("Total stations: " + allstns.size());
+      
+      System.out.println("Total global stations: " + allstns.size());
       
       Scanner sc = new Scanner(System.in);
-      System.out.println("Enter a state abbreviation: ");
+      System.out.println("Enter a state abbreviation (e.g., CA): ");
       String state = sc.next();
-      System.out.println("Stations in " + state);
+      
+      // Track the southernmost station
+      WeatherStation southernmost = allstns.get(0); 
+      int count = 0; 
+      
+      System.out.println("Stations in " + state + ":");
+      
       for (WeatherStation ws : allstns) {
+         // Existing State Filter Logic
          if (ws.isLocatedInState(state)) {
             System.out.println("  " + ws.getId() + ": " + ws.getName());
+            count++;
+         }
+         
+         // New Southernmost Logic: Compare latitudes
+         if (ws.getLat() < southernmost.getLat()) {
+            southernmost = ws;
          }
       }
+      
+      System.out.println("---------------------------------------");
+      System.out.println("Total stations found in " + state + ": " + count);
+      System.out.println("Southernmost station: " + southernmost.getName() + 
+                         " (Lat: " + southernmost.getLat() + ")");
+      System.out.println("---------------------------------------");
+      sc.close();
    }
 }
